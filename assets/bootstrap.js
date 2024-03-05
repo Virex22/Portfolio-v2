6ecf07e5ee9f65354ed82f31cca5850de74d0756
Vincent REMY <vincentremy222@gmail.com>
2024-03-05 22:09:41 +0100

    ✨feat: create footer and maintenance page

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,5 @@
 // import { startStimulusApp } from '@symfony/stimulus-bundle';
 //
 // const app = startStimulusApp();
-// register any custom, 3rd party controllers here
+// register any custom, 3rd party js here
 // app.register('some_controller_name', SomeImportedController);

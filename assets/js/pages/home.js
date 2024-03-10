c054be1581387197612147519c2e99c4b438bb92
Vincent REMY <vincentremy222@gmail.com>
2024-03-11 00:35:29 +0100

    🚧wip: services section

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -6,11 +6,12 @@ import 'swiper/css/effect-fade';
 /* Swiper */
 
 import Swiper from 'swiper';
-import {Pagination, Autoplay, EffectFade} from 'swiper/modules';
+import {Pagination, Autoplay, EffectFade, Navigation} from 'swiper/modules';
 
-Swiper.use([Pagination, Autoplay, EffectFade]);
+Swiper.use([Pagination, Autoplay, EffectFade, Navigation]);
 
-new Swiper(".mySwiper", {
+/* Swiper for hero section */
+new Swiper(".hero-swiper", {
     loop: true,
     effect: "fade",
     fadeEffect: {
@@ -23,5 +24,20 @@ new Swiper(".mySwiper", {
     speed: 1000,
     pagination: {
         el: ".swiper-pagination",
+    },
+});
+
+/* Swiper for services section */
+
+new Swiper(".services-swiper", {
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: "auto",
+    speed: 250,
+    slidesOffsetBefore: 50,
+    slidesOffsetAfter: 50,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
 });

216a319762c15e202fb137cc27e840b84f12903a
Vincent REMY <vincentremy222@gmail.com>
2024-03-10 12:52:46 +0100

    ✨feat: create hero with slider banner and fade effect

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -2,19 +2,24 @@ import "../../styles/pages/home.scss";
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+import 'swiper/css/effect-fade';
 /* Swiper */
 
 import Swiper from 'swiper';
-import {Pagination, Autoplay} from 'swiper/modules';
+import {Pagination, Autoplay, EffectFade} from 'swiper/modules';
 
-Swiper.use([Pagination, Autoplay]);
+Swiper.use([Pagination, Autoplay, EffectFade]);
 
 new Swiper(".mySwiper", {
     loop: true,
-    // autoplay: {
-    //     delay: 4000,
-    //     disableOnInteraction: false,
-    // },
+    effect: "fade",
+    fadeEffect: {
+        crossFade: true,
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
     speed: 1000,
     pagination: {
         el: ".swiper-pagination",

721c85462c4a1cdb800002140666c4d2b8c85793
Vincent REMY <vincentremy222@gmail.com>
2024-03-13 23:36:46 +0100

    ✨feat: add Contact Message fixtures

---
+++ src/DataFixtures/Entity/ContactMessageFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures\Entity;
+
+use App\Entity\ContactMessage;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ContactMessageFixtures extends Fixture
+{
+    static int $count = 5;
+    public function getOrder(): int
+    {
+        return 3;
+    }
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 1; $i <= self::$count; $i++) {
+            $contactMessage = new ContactMessage();
+            $contactMessage->setName('Name ' . $i);
+            $contactMessage->setEmail('email' . $i . '@example.com');
+            $contactMessage->setMessage('Message ' . $i);
+            $contactMessage->setSubject('Subject ' . $i);
+            $contactMessage->setSurname('Surname ' . $i);
+            $manager->persist($contactMessage);
+        }
+        $manager->flush();
+    }
+
+}

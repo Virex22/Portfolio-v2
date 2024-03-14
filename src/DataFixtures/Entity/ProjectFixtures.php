cee4addf05c1f64675ea7851619619ddbac02679
Vincent REMY <vincentremy222@gmail.com>
2024-03-14 23:57:52 +0100

    ✨feat: create Project fixtures

---
+++ src/DataFixtures/Entity/ProjectFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures\Entity;
+
+use App\Entity\Project;
+use App\Entity\Skill;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ProjectFixtures extends Fixture
+{
+    static int $count = 10;
+
+    public function getDependencies() : array
+    {
+        return [
+            SkillFixtures::class
+        ];
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $allSkills = $manager->getRepository(Skill::class)->findAll();
+        for ($i = 1; $i <= self::$count; $i++) {
+            $project = new Project();
+            $project->setName('Project ' . $i);
+            $project->setDescription('Description ' . $i);
+            $project->setContent('Content ' . $i);
+
+            for ($j = 1; $j <= rand(1, 3); $j++)
+                if (count($allSkills) > self::$count - $i)
+                    $project->addSkill(array_pop($allSkills));
+            $manager->persist($project);
+        }
+        $manager->flush();
+    }
+
+}

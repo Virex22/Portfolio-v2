d1ff118757830f84447fb0bd9b48b5428ffc750d
Vincent REMY <vincentremy222@gmail.com>
2024-04-04 23:37:48 +0200

    ✨feat: project template part

--- assets/js/pages/projects.js
+++ assets/js/pages/projects.js
@@ -1,1 +1,26 @@
 import "../../styles/pages/projects.scss";
+import TomSelect from 'tom-select';
+
+/* Select part */
+let elems  = document.querySelectorAll('.select-box input[type="checkbox"].options-view-button');
+
+elems.forEach((elem) => {
+    //on checked we remove checked of other checkboxes
+    elem.addEventListener('change', function() {
+        if (this.checked) {
+            elems.forEach((el) => {
+                if (el !== this) {
+                    el.checked = false;
+                }
+            });
+        }
+    });
+});
+
+/* Selectize part */
+const select = document.querySelectorAll('.tom-select');
+select.forEach((elem) => {
+    new TomSelect(elem, {
+        mode: 'single',
+    });
+});
--- src/Controller/ProjectsController.php
+++ src/Controller/ProjectsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,10 @@ use Symfony\Component\Routing\Attribute\Route;
 class ProjectsController extends AbstractController
 {
     #[Route('/projects', name: 'portfolio_projects')]
-    public function index(): Response
+    public function index(ProjectRepository $projectRepository): Response
     {
-        return $this->render('pages/projects/projects.html.twig');
+        return $this->render('pages/projects/projects.html.twig', [
+            'projects' => $projectRepository->findAll(),
+        ]);
     }
 }

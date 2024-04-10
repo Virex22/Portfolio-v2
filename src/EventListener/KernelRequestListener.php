be4b98264e77a43e4734ccc17bbf9218a1bee266
Vincent REMY <vincentremy222@gmail.com>
2024-04-10 22:34:12 +0200

    ✨feat: disable maintenance in back office

--- src/EventListener/KernelRequestListener.php
+++ src/EventListener/KernelRequestListener.php
@@ -6,6 +6,9 @@ use App\Helper\ConfigurationHelper;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Csrf\TokenStorage\TokenStorageInterface;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Twig\Environment;
 
 class KernelRequestListener
@@ -23,6 +26,12 @@ class KernelRequestListener
     #[AsEventListener(event: RequestEvent::class, priority: 5000)]
     public function onKernelRequest(RequestEvent $event): void
     {
+        foreach ($this->getExcludeRoutes() as $route) {
+            if (str_starts_with($event->getRequest()->getPathInfo(), "/" . $route)) {
+                return;
+            }
+        }
+
         if (!$this->configurationHelper->existConfiguration(self::$maintenanceConfigKey)) {
             $this->configurationHelper->setConfiguration(self::$maintenanceConfigKey, 'false');
             return;
@@ -36,5 +45,14 @@ class KernelRequestListener
 
         $event->setResponse($response);
         $event->stopPropagation();
+    }
+
+    private function getExcludeRoutes(): array
+    {
+        return [
+            $_ENV['ADMIN_PATH'],
+            "_profiler",
+            "_fragment",
+        ];
     }
 }

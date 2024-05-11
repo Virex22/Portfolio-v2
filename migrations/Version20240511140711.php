0cab59f8ee6279f1c82788e950320315cd39cdde
Vincent REMY <vincentremy222@gmail.com>
2024-05-11 17:12:26 +0200

    🚀cd: work with symlink to make MEP more properly (and restart migration to restart at 0)

--- migrations/Version20240511140711.php
+++ migrations/Version20240511140711.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240314171637 extends AbstractMigration
+final class Version20240511140711 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,18 +20,22 @@ final class Version20240314171637 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE administrator (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_58DF0651F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE configuration (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, value LONGTEXT NOT NULL, date_add DATETIME NOT NULL, date_update DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE contact_message (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, subject VARCHAR(255) NOT NULL, message LONGTEXT NOT NULL, name VARCHAR(255) DEFAULT NULL, surname VARCHAR(255) DEFAULT NULL, date_add DATETIME NOT NULL, date_update DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE experience (id INT AUTO_INCREMENT NOT NULL, compagny_name VARCHAR(100) NOT NULL, post_name VARCHAR(100) NOT NULL, start_date DATETIME NOT NULL, end_date DATETIME DEFAULT NULL, compangy_logo_url LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE formation (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, description LONGTEXT NOT NULL, start_date DATETIME NOT NULL, end_date DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE project (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, description LONGTEXT NOT NULL, content LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE service (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, priority SMALLINT NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE skill (id INT AUTO_INCREMENT NOT NULL, skill_groups_id INT DEFAULT NULL, name VARCHAR(100) NOT NULL, badge_url LONGTEXT DEFAULT NULL, description LONGTEXT DEFAULT NULL, INDEX IDX_5E3DE4775CC60692 (skill_groups_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE experience (id INT AUTO_INCREMENT NOT NULL, location LONGTEXT NOT NULL, start_date DATETIME NOT NULL, end_date DATETIME DEFAULT NULL, logo VARCHAR(255) DEFAULT NULL, updated_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE formation (id INT AUTO_INCREMENT NOT NULL, location LONGTEXT NOT NULL, start_date DATETIME NOT NULL, logo VARCHAR(255) DEFAULT NULL, end_date DATETIME DEFAULT NULL, updated_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE project (id INT AUTO_INCREMENT NOT NULL, start_date DATE DEFAULT NULL, end_date DATE DEFAULT NULL, cover_image VARCHAR(255) DEFAULT NULL, updated_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE project_content (id INT AUTO_INCREMENT NOT NULL, project_id INT NOT NULL, position INT NOT NULL, view_type VARCHAR(50) NOT NULL, image VARCHAR(255) DEFAULT NULL, updated_at DATETIME DEFAULT NULL, INDEX IDX_68DB3CCC166D1F9C (project_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE service (id INT AUTO_INCREMENT NOT NULL, priority SMALLINT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE skill (id INT AUTO_INCREMENT NOT NULL, skill_groups_id INT DEFAULT NULL, badge_url LONGTEXT DEFAULT NULL, type VARCHAR(50) NOT NULL, updated_at DATETIME DEFAULT NULL, INDEX IDX_5E3DE4775CC60692 (skill_groups_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE skill_formation (skill_id INT NOT NULL, formation_id INT NOT NULL, INDEX IDX_13B11AE25585C142 (skill_id), INDEX IDX_13B11AE25200282E (formation_id), PRIMARY KEY(skill_id, formation_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE skill_experience (skill_id INT NOT NULL, experience_id INT NOT NULL, INDEX IDX_10191D715585C142 (skill_id), INDEX IDX_10191D7146E90E27 (experience_id), PRIMARY KEY(skill_id, experience_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE skill_project (skill_id INT NOT NULL, project_id INT NOT NULL, INDEX IDX_35464EC75585C142 (skill_id), INDEX IDX_35464EC7166D1F9C (project_id), PRIMARY KEY(skill_id, project_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE skill_group (id INT AUTO_INCREMENT NOT NULL, custom_name VARCHAR(100) DEFAULT NULL, acquired_percentage SMALLINT NOT NULL, priority SMALLINT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE skill_group (id INT AUTO_INCREMENT NOT NULL, acquired_percentage SMALLINT NOT NULL, priority SMALLINT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE translation (id INT AUTO_INCREMENT NOT NULL, locale VARCHAR(10) NOT NULL, domain VARCHAR(255) NOT NULL, entity_id INT NOT NULL, value LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE project_content ADD CONSTRAINT FK_68DB3CCC166D1F9C FOREIGN KEY (project_id) REFERENCES project (id)');
         $this->addSql('ALTER TABLE skill ADD CONSTRAINT FK_5E3DE4775CC60692 FOREIGN KEY (skill_groups_id) REFERENCES skill_group (id)');
         $this->addSql('ALTER TABLE skill_formation ADD CONSTRAINT FK_13B11AE25585C142 FOREIGN KEY (skill_id) REFERENCES skill (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE skill_formation ADD CONSTRAINT FK_13B11AE25200282E FOREIGN KEY (formation_id) REFERENCES formation (id) ON DELETE CASCADE');
@@ -44,6 +48,7 @@ final class Version20240314171637 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE project_content DROP FOREIGN KEY FK_68DB3CCC166D1F9C');
         $this->addSql('ALTER TABLE skill DROP FOREIGN KEY FK_5E3DE4775CC60692');
         $this->addSql('ALTER TABLE skill_formation DROP FOREIGN KEY FK_13B11AE25585C142');
         $this->addSql('ALTER TABLE skill_formation DROP FOREIGN KEY FK_13B11AE25200282E');
@@ -51,17 +56,20 @@ final class Version20240314171637 extends AbstractMigration
         $this->addSql('ALTER TABLE skill_experience DROP FOREIGN KEY FK_10191D7146E90E27');
         $this->addSql('ALTER TABLE skill_project DROP FOREIGN KEY FK_35464EC75585C142');
         $this->addSql('ALTER TABLE skill_project DROP FOREIGN KEY FK_35464EC7166D1F9C');
+        $this->addSql('DROP TABLE administrator');
         $this->addSql('DROP TABLE configuration');
         $this->addSql('DROP TABLE contact_message');
         $this->addSql('DROP TABLE experience');
         $this->addSql('DROP TABLE formation');
         $this->addSql('DROP TABLE project');
+        $this->addSql('DROP TABLE project_content');
         $this->addSql('DROP TABLE service');
         $this->addSql('DROP TABLE skill');
         $this->addSql('DROP TABLE skill_formation');
         $this->addSql('DROP TABLE skill_experience');
         $this->addSql('DROP TABLE skill_project');
         $this->addSql('DROP TABLE skill_group');
+        $this->addSql('DROP TABLE translation');
         $this->addSql('DROP TABLE messenger_messages');
     }
 }

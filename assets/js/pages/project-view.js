29b749b60812d0835da9766bb918f87af7e70e96
Vincent REMY <vincentremy222@gmail.com>
2024-06-10 23:45:43 +0200

    ✨feat: add fancybox view for project images

--- assets/js/pages/project-view.js
+++ assets/js/pages/project-view.js
@@ -1,6 +1,7 @@
 import "../../styles/pages/project/project-view.scss";
 import "../utility/navbar";
 import 'aos/dist/aos.css';
+import '../utility/fancybox';
 import AOS from 'aos';
 
 AOS.init();
---
+++ assets/js/utility/fancybox.js
@@ -0,0 +1,8 @@
+import {Fancybox} from "@fancyapps/ui";
+
+//include css
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+document.addEventListener('DOMContentLoaded', function() {
+    Fancybox.bind("[data-fancybox]", {});
+});
